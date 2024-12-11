82f050948dab8a8a029343daf83656ceeabfa80e
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-12-10 18:00:40 -0800

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Арсений Корниенко on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
